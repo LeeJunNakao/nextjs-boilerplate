caf38d57c04efdbaeaae4d2b8f76fe7e8f5bc5c7
Lee Jun <leejun.nakao@gmail.com>
2022-05-11 22:39:04 +0000

    feat: create boilerplate

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.tsx",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "staticDirs": ["../public"]
+}
